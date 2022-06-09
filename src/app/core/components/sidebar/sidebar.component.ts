f31a7fab62c5c89bac71b2938cbd91616d10480d
Leonardo Leyva <lleyva@intersog.com>
2022-06-09 07:37:06 -0600

    Set selected icons from sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UrlPathRole } from '../../type';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  urlPath: string = '';
+  isAdminActive: boolean = false;
+  isCoachActive: boolean = false;
+  isRefereeActive: boolean = false;
 
-  constructor() { }
+  constructor(private route: Router) {}
 
   ngOnInit(): void {
-  }
+    this.route.events.subscribe((ev: any) => {
+      if (!ev.routerEvent) return;
+      this.urlPath = ev.routerEvent.url;
+      const role = this.urlPath.split('/')[1] as UrlPathRole;
 
+      this.isAdminActive = role === 'admin';
+      this.isCoachActive = role === 'coach';
+      this.isRefereeActive = role === 'referee';
+    });
+  }
 }
---
+++ src/app/core/type.ts
@@ -0,0 +1,1 @@
+export type UrlPathRole = 'admin' | 'coach' | 'referee'
